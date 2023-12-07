62541caf707c15dd6f8950231a3f6c3efc295397
min9seok <wjdndd9319@naver.com>
2023-12-07 17:31:23 +0900

    타겟제거 테스트

--- src/main/java/org/pro/pulmuone/HomeController.java
+++ src/main/java/org/pro/pulmuone/HomeController.java
@@ -1,42 +1,28 @@
 package org.pro.pulmuone;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
+import java.util.List;
 
-import org.pro.pulmuone.mapper.faq.FaqMapper;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.pro.pulmuone.domain.product.ProductsDTO;
+import org.pro.pulmuone.mapper.main.MainMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
-/**
- * Handles requests for the application home page.
- */
 @Controller
+@RequestMapping("/")
 public class HomeController {
 	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
 	@Autowired
-	private FaqMapper faqMapper;
+	private MainMapper mainMapper;
 	
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "home";
+	public HomeController() {
+		super();
 	}
-
+	@RequestMapping("index")
+	public String home(Model model) throws Exception {
+		List<ProductsDTO> mainbestlist = this.mainMapper.get();		
+		model.addAttribute("mainbestlist", mainbestlist );
+		return "home.index";
+	}
 }
